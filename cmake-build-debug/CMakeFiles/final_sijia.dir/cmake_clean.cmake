2e534d324dd7346a82803a1c9de9e9e8d2c10d70
sjzScarlett <33424654+sjzScarlett@users.noreply.github.com>
2017-12-14 23:22:08 -0500

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/final_sijia.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/final_sijia.dir/main.cpp.o"
+  "final_sijia.pdb"
+  "final_sijia"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/final_sijia.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
